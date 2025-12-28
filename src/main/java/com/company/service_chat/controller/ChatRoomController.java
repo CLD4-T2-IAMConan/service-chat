0df70bee8db041288253eb96d4eeb07da317411a
daeunkim701 <lionboot2025@gmail.com>
2025-12-28 22:39:05 +0900

    fix: system-action api userId 프론트 요청에 맞게 수정

--- src/main/java/com/company/service_chat/controller/ChatRoomController.java
+++ src/main/java/com/company/service_chat/controller/ChatRoomController.java
@@ -4,8 +4,10 @@ import com.company.service_chat.dto.*;
 import com.company.service_chat.service.ChatRoomService;
 import com.company.service_chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @RestController
 @RequestMapping("/chat/rooms")
@@ -63,8 +65,10 @@ public class ChatRoomController {
     @PostMapping("/system-action")
     public void handleSystemAction(
             @RequestBody SystemActionRequest request,
-            @RequestParam Long userId
+            Authentication authentication
     ) {
+        Long userId = Long.valueOf(authentication.getName());
+
         switch (request.getActionCode()) {
             case "TRANSFER_REQUEST":
                 chatRoomService.handleDealRequest(
